c3b32e86045265c5f4e50a8df26c617c5ebfb287
minhazulabidin <minhajulabedin648@gmail.com>
2026-08-16 14:29:20 +0600

    css color added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
-  )
-}
+    <section className="flex flex-col md:flex-row gap-4 p-4 ">
+      {/* left */}
+      <div className="w-full lg:w-2/3">
+        <div>
+          <UserCard type="admin" />
+          <UserCard type="teacher" />
+          <UserCard type="student" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full lg:w-1/3"></div>
+    </section>
+  );
+};
 
-export default AdminPage
+export default AdminPage;
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserCard = ({type}: {type: string}) => {
+  return (
+    <div>UserCard - {type}</div>
+  )
+}
+
+export default UserCard
