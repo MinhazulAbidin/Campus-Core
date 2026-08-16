fb9c617d091e0ec68f099d7782b0d9bd7144f108
minhazulabidin <minhajulabedin648@gmail.com>
2026-08-16 11:58:32 +0600

    menu done and navbar create

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,7 +8,10 @@ export default function DashboardLayout({ children }: LayoutProps<"/">) {
     <div className="flex min-h-screen">
       {/* left */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
-        <Link href="/" className="flex gap-2 items-end justify-center lg:justify-start">
+        <Link
+          href="/"
+          className="flex gap-2 items-end justify-center lg:justify-start"
+        >
           <Image
             src="/logo.png"
             alt="logo"
@@ -20,7 +24,10 @@ export default function DashboardLayout({ children }: LayoutProps<"/">) {
         <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">right</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
