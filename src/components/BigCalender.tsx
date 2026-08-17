857f02bce571e2adc486ef00d41751a66740e467
minhazulabidin <minhajulabedin648@gmail.com>
2026-08-17 16:35:27 +0600

    done

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useState } from "react";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+const BigCalender = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+
+  const handleViewChange = (newView: View) => {
+    setView(newView);
+  };
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={calendarEvents}
+        startAccessor="start"
+        endAccessor="end"
+        views={["work_week", "day"]}
+        view={view}
+        onView={handleViewChange}
+        style={{ height: "98%" }}
+      />
+    </div>
+  );
+};
+
+export default BigCalender;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,21 +2,35 @@ import React from "react";
 import menuItems from "../../data/menuItems";
 import Link from "next/link";
 import Image from "next/image";
+import { role } from "@/lib/data";
 
 const Menu = () => {
   return (
     <div className="mt-4 text-sm">
       {menuItems.map((item, index) => (
         <div key={index} className="flex flex-col gap-2 ">
-          <span className="hidden lg:block text-gray-400 font-light my-4">{item.title}</span>
-          {item.items.map((subItem, subIndex) => (
-            <div key={subIndex}>
-              <Link href={subItem?.href} className="flex gap-4 items-center justify-center lg:justify-start text-gray-500 py-2 hover:shadow-md rounded hover:scale-x-110 duration-300">
-              <Image priority src={subItem?.icon} alt={subItem?.label} width={20} height={20}/>
-                <span className="hidden lg:block">{subItem?.label}</span>
-              </Link>
-            </div>
-          ))}
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {item.title}
+          </span>
+          {item.items.map((subItem, subIndex) => {
+            if (subItem.visible.includes(role))
+              return (
+                <Link
+                  key={subIndex}
+                  href={subItem?.href}
+                  className="flex gap-4 items-center justify-center lg:justify-start text-gray-500 py-2 md:px-2 hover:shadow-md rounded-md hover:bg-lamaSkyLight hover:scale-x-110 duration-200"
+                >
+                  <Image
+                    priority
+                    src={subItem?.icon}
+                    alt={subItem?.label}
+                    width={20}
+                    height={20}
+                  />
+                  <span className="hidden lg:block">{subItem?.label}</span>
+                </Link>
+              );
+          })}
         </div>
       ))}
     </div>
